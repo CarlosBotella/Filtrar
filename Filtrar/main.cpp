9f34f8e2513e0aa8117988619cdbfed0c379c4bb
cbotella <carlosb.n@icloud.com>
2021-10-21 11:48:08 +0200

    Initial Commit

---
+++ Filtrar/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Filtrar
+//
+//  Created by Carlos Botella Navarro on 21/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

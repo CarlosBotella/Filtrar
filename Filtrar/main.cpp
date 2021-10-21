4892a616367b6badfcca9adfe1126cf3c770406d
cbotella <carlosb.n@icloud.com>
2021-10-21 12:25:49 +0200

    Filtrar 1.0

--- Filtrar/main.cpp
+++ Filtrar/main.cpp
@@ -5,10 +5,28 @@
 //  Created by Carlos Botella Navarro on 21/10/21.
 //
 
+//------------------------------------------------
+// Lista <R> --> filtrar() --> Lista <R>
+//------------------------------------------------
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int filtrar(int *p, int *m, int cuantos){
+    int num = 0;
+    for (int i = 0; i < cuantos; i++) {
+        if (p[i] >= 0){
+            m[num] = p[i];
+            num  = num + 1;
+        }
+    }
+    return num;
+}
+
+int main() {
+    int lista[10] = {-10,4,7,-8,20,-35,6,3,-2,9};
+    int listaPositivos[10];
+    int numElementos = filtrar(&lista[0], &listaPositivos[0],10);
+    for (int i = 0; i < numElementos; i++) {
+        cout << listaPositivos[i] << endl;
+    }
 }
